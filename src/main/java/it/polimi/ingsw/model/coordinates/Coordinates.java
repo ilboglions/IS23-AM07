6d582cdf02c3a861f9c9042b61db1e8953a53e05
TrueMaicol <michi.cavicchio@hotmail.com>
2023-03-16 10:43:39 +0100

    Coordinates class done

---
+++ src/main/java/it/polimi/ingsw/model/coordinates/Coordinates.java
@@ -0,0 +1,19 @@
+package it.polimi.ingsw.model.coordinates;
+
+public class Coordinates {
+
+    private final int x,y;
+
+    public Coordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
