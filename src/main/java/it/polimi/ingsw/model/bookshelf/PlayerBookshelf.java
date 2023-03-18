fce85a43214fef3840e42bc8d4ebef5ad21de54c
Creix <christian.biffi7@gmail.com>
2023-03-18 11:40:03 +0100

    Corrected if condition

--- src/main/java/it/polimi/ingsw/model/bookshelf/PlayerBookshelf.java
+++ src/main/java/it/polimi/ingsw/model/bookshelf/PlayerBookshelf.java
@@ -7,7 +7,7 @@ import it.polimi.ingsw.model.tiles.ItemTile;
 public class PlayerBookshelf extends Bookshelf{
 
     public void insertItemTile(Coordinates c, ItemTile tile) throws NotEmptyException {
-        if (this.tiles[c.getX()][c.getY()] != null)
+        if (this.tiles[c.getX()][c.getY()] == null)
             this.tiles[c.getX()][c.getY()] = tile;
         else
             throw new NotEmptyException("This tile is already assigned!");
